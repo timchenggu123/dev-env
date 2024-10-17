55c03ef8bd383b06657b3c7373b6fe406c2f17fb
Tim Gu <2013tim.g@gmail.com>
2024-10-17 19:13:47 +0000

    Added plug support for nvim

---
+++ nvim/init_ext
@@ -0,0 +1,6 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+vim.call('plug#end')
--- nvim/install.sh
+++ nvim/install.sh
@@ -5,8 +5,16 @@ sudo tar -C /opt -xzf nvim-linux64.tar.gz
 echo export PATH="$PATH:/opt/nvim-linux64/bin" > ~/.bashrc
 export PATH="$PATH:/opt/nvim-linux64/bin"
 ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
+rm ./nvim-linux64.tar.gz
 
 rm -rf ~/.config/nvim
 git clone https://github.com/NvChad/starter ~/.config/nvim 
 nvim --headless "+Lazy sync" +qall
 rm -rf ~/.config/.git
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cat ./init_ext >> ~/.config/nvim/init.lua
+nvim --headless "+PlugInstall" +qall
+
+
