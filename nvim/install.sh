4d5a9191e15052d290a63ee184f9758f18b8c3b7
Tim Gu <2013tim.g@gmail.com>
2024-10-17 18:08:36 +0000

    Updated nvim install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,6 +4,7 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 echo export PATH="$PATH:/opt/nvim-linux64/bin" > ~/.bashrc
 export PATH="$PATH:/opt/nvim-linux64/bin"
+ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
 
 rm -rf ~/.config/nvim
 git clone https://github.com/NvChad/starter ~/.config/nvim 
